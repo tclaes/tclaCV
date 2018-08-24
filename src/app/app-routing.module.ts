f716a87f33638a0db28365f17f0b096d680ae9a5
Tom <tom.claes82@gmail.com>
2018-08-24 15:33:07 +0200

    layout aangepast

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
+  {path: 'login', component: HomeComponent},
   {path: 'todo', component: TodoListComponent, canActivate: [AuthGuard]}
 ];
 
